6be750559f5a6ae4b0c4ff5d788702eb50091ff1
rsmudge <rsmudge@957f4724-0442-2941-0443-fd1ab383608d>
2011-03-10 02:52:15 +0000

    removing Armitage Windows package (it's coming back in a second)

--- release/armitage-windows/armitage.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set BASE=%~dp0
-cd "%BASE%"
-set PATH=%BASE%ruby\bin;%BASE%java\bin;%BASE%tools;%BASE%tools\svn\bin;%BASE%nmap;%BASE%postgresql\bin;%PATH%
-set JAVA_HOME=%BASE%java
-cd "%BASE%msf3"
-start rubyw armitage -y "%BASE%config\database.yml"
