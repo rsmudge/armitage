8fa2f315fec6a12b1c2930d0563baae30c4e6919
rsmudge <rsmudge@957f4724-0442-2941-0443-fd1ab383608d>
2011-03-10 02:45:40 +0000

    added Armitage app file for MacOS X.

---
+++ trunk/dist/mac/build.sh
@@ -0,0 +1,14 @@
+#
+# unzip armitage.tgz in this directory first.
+#
+
+rm -rf dist
+mkdir dist
+cp -r Armitage.app dist
+cp armitage/armitage.jar dist/Armitage.app/Contents/Resources/Java
+cp armitage/*.txt dist/
+cp *.rtf dist/
+rm -rf armitage
+mv dist Armitage
+hdiutil create -ov -volname Armitage -srcfolder ./Armitage armitage.dmg
+rm -rf armitage
