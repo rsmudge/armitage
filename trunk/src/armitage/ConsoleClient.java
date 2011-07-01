e3c5bb9f23262674c0d7d3a8d43a8d06d0050e80
rsmudge <rsmudge@957f4724-0442-2941-0443-fd1ab383608d>
2011-07-01 17:18:17 +0000

    removed a work-around for an old msf bug (that appears fixed).

--- trunk/src/armitage/ConsoleClient.java
+++ trunk/src/armitage/ConsoleClient.java
@@ -130,8 +130,8 @@ public class ConsoleClient implements Runnable, ActionListener {
 
 			read = readResponse();
 			if ("false".equals(read.get("busy") + "") && "".equals(read.get("data") + "")) {
-				System.err.println("Resending command: " + text + " -- " + read);
-				connection.execute(writeCommand, new Object[] { session, Base64.encode(text) });
+				System.err.println("Should command be resent? " + text + " -- " + read);
+				//connection.execute(writeCommand, new Object[] { session, Base64.encode(text) });
 			}
 			else {
 				processRead(read);
