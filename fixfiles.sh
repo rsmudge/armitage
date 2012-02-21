327184b3551c826b74fae4466a6f0ad539464313
rsmudge <rsmudge@957f4724-0442-2941-0443-fd1ab383608d>
2012-02-21 16:49:02 +0000

    changes.

--- fixfiles.sh
+++ fixfiles.sh
@@ -2,7 +2,5 @@
 # fix the line formats so windows users can read the files too
 #
 perl -pi -e 's/\n/\r\n/g' readme.txt
-perl -pi -e 's/\n/\r\n/g' license.txt
-perl -pi -e 's/\n/\r\n/g' dist/windows/armitage-debug.bat
 perl -pi -e 's/\n/\r\n/g' whatsnew.txt
 
