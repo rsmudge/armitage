cd8bf35c177685cecedb71b6c3cb8b477d63f3c7
rsmudge <rsmudge@957f4724-0442-2941-0443-fd1ab383608d>
2012-05-04 03:45:43 +0000

    refactored tab completion.

--- src/armitage/TabCompletion.java
+++ src/armitage/TabCompletion.java
@@ -7,93 +7,35 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+import java.io.IOException;
+
 /* A generic class to manage reading/writing to a console. Keeps the code simpler (although the Sleep code to do this is 
    simpler than this Java code. *sigh* */
-public class TabCompletion {
+public class TabCompletion extends GenericTabCompletion {
 	protected RpcConnection connection;
-	protected Console       window;
 	protected String        session;
 	protected String        tabsCommand;
 
-	/* state for the actual tab completion */
-	protected String last      = null;
-	protected Iterator tabs    = null;
-
-
-
-	public Console getWindow() {
-		return window;
-	}
-
 	public TabCompletion(Console window, RpcConnection connection, String session, String tabsCommand) {
-		this.window = window;
+		super(window);
 		this.connection = connection;
 		this.session = session;
 		this.tabsCommand = tabsCommand;
-
-		window.addActionForKey("pressed TAB", new AbstractAction() {
-			public void actionPerformed(ActionEvent ev) {
-				tabComplete(ev);
-			}
-		});
 	}
 
-	public void tabComplete(ActionEvent ev) {
-		String text = window.getInput().getText();
-		if (text.length() == 0)
-			return;
+	public Collection getOptions(String text) {
+		try {
+			Map response = (Map)connection.execute(tabsCommand, new Object[] { session, text });
 
-		if (tabs != null && tabs.hasNext() && text.equals(last)) {
-			last = (String)tabs.next();
-			window.getInput().setText(last);
+			if (response.get("tabs") == null)
+				return null;
+
+			Collection options = (Collection)response.get("tabs");
+			return options;
 		}
-		else {
-			try {
-				Map response = (Map)connection.execute(tabsCommand, new Object[] { session, text });
-
-				if (response.get("tabs") == null)
-					return;
-
-				LinkedHashSet responses = new LinkedHashSet();
-
-				/* cycle through all of our options, we want to split items up to the
-				   first slash. We also want them to be unique and ordered (hence the
-				   linked hash set */
-				Collection options = (Collection)response.get("tabs");
-				Iterator i = options.iterator();
-				while (i.hasNext()) {
-					String option = i.next() + "";
-
-					String begin;
-					String end;
-
-					if (text.length() > option.length()) {
-						begin = option;
-						end = "";
-					}
-					else {
-						begin = option.substring(0, text.length());
-						end = option.substring(text.length());
-					}
-
-					int nextSlash;
-					if ((nextSlash = end.indexOf('/')) > -1 && (nextSlash + 1) < end.length()) {
-						end = end.substring(0, nextSlash);
-					}
-
-					responses.add(begin + end);
-				}
-
-				responses.add(text);
-
-				tabs = responses.iterator();
-				last = (String)tabs.next();
-
-				window.getInput().setText(last);
-			}
-			catch (Exception ex) {
-				ex.printStackTrace();
-			}
+		catch (IOException ioex) {
+			ioex.printStackTrace();
 		}
+		return null;
 	}
 }
