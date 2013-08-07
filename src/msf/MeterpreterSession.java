62388d4584a6cf70bb41ec0b7d430210b4c4fe68
rsmudge <rsmudge@957f4724-0442-2941-0443-fd1ab383608d>
2013-08-07 01:55:55 +0000

    small patch to enumdesktops command.

--- src/msf/MeterpreterSession.java
+++ src/msf/MeterpreterSession.java
@@ -207,6 +207,10 @@ public class MeterpreterSession implements Runnable {
 				readUntilSuccessful(c, false);
 				return;
 			}
+			else if (c.text.startsWith("enumdesktops") && !teammode) {
+				readUntilSuccessful(c, false);
+				return;
+			}
 
 			//System.err.println("(" + session + ") latency: " + (System.currentTimeMillis() - c.start) + " -- " + c.text);
 
