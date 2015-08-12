da96611d62b7941edf3d7272f8cce32503aa74ba
Raphael Mudge <rsmudge@gmail.com>
2015-08-12 18:59:18 -0400

    speed up rendering of graph view.

--- src/graph/NetworkGraph.java
+++ src/graph/NetworkGraph.java
@@ -62,14 +62,6 @@ public class NetworkGraph extends JComponent implements ActionListener, Refresha
 			getVerticalScrollBar().setBlockIncrement(60);
 		}
 
-		public void paint(Graphics g) {
-			Graphics2D g2 = (Graphics2D)g;
-			g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-			g2.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
-			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-			super.paint(g2);
-		}
-
 		public mxInteractiveCanvas createCanvas() {
 			return new NetworkGraphCanvas();
 		}
