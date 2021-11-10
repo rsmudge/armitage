b1c84ab4905ab6268293defa2506026ade07f7be
Nadeem Douba <ndouba@redcanari.com>
2021-11-10 15:29:35 +0000

    Updated build and clean scripts

--- package.sh
+++ package.sh
@@ -3,3 +3,24 @@
 set -ex
 
 ./gradlew assemble
+
+
+for i in unix windows mac; do
+
+  if [ "${i}" == "mac" ] && [ "$(uname)" != "Darwin" ]; then
+    echo "Skipping macOS build because this is not running on Darwin"
+    continue
+  fi
+
+  mkdir -p "release/${i}"
+  cp *.txt "release/${i}"
+  cp build/*.jar "release/${i}"
+  cp -r "dist/${i}/"* "release/${i}"
+
+  if [ "${i}" == "mac" ] && [ "$(uname)" == "Darwin" ]; then
+    pushd "release/${i}"
+    ./build.sh
+    popd
+  fi
+
+done;
